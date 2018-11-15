5e39cffeff8364df3399081667c8004d9e39aa9e
sammui <sammui1223@gmail.com>
2018-11-16 04:25:50 +0800

    Updates

--- precache-manifest.d32c950ee2e31bcf75d411405bb6da25.js
+++ precache-manifest.d32c950ee2e31bcf75d411405bb6da25.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "a4a5cfc67aae0c8e170b",
+    "revision": "827060d845722b57b228",
     "url": "/wuowuo-dinner-time/static/css/main.6db940a4.chunk.css"
   },
   {
-    "revision": "a4a5cfc67aae0c8e170b",
-    "url": "/wuowuo-dinner-time/static/js/main.a4a5cfc6.chunk.js"
+    "revision": "827060d845722b57b228",
+    "url": "/wuowuo-dinner-time/static/js/main.827060d8.chunk.js"
   },
   {
     "revision": "29e4dff59da53bd2d7fd",
@@ -20,19 +20,19 @@ self.__precacheManifest = [
     "url": "/wuowuo-dinner-time/static/js/runtime~main.322d13e5.js"
   },
   {
-    "revision": "99866c9af7ebdddc786853a3e7380597",
-    "url": "/wuowuo-dinner-time/static/media/upload-hover.99866c9a.png"
-  },
-  {
     "revision": "75c39826dd85510d3b91d163e728a4fa",
     "url": "/wuowuo-dinner-time/static/media/upload-orig.75c39826.png"
+  },
+  {
+    "revision": "99866c9af7ebdddc786853a3e7380597",
+    "url": "/wuowuo-dinner-time/static/media/upload-hover.99866c9a.png"
   },
   {
     "revision": "975a00af7dbb6dc3deecdaef359cb4af",
     "url": "/wuowuo-dinner-time/static/media/dark_cat.975a00af.png"
   },
   {
-    "revision": "af3bffc67f594723f52881565cbf9080",
+    "revision": "21734016013107f9838c92d629a4d41d",
     "url": "/wuowuo-dinner-time/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wuowuo-dinner-time/precache-manifest.f21d196bae0809900332bc138c69c1e6.js"
+  "/wuowuo-dinner-time/precache-manifest.d32c950ee2e31bcf75d411405bb6da25.js"
 );
 
 workbox.clientsClaim();
